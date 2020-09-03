42c0a1f260bc9904c8ca45b8fcf5c92c4adc9e2c
Joechencc <55113116+Joechencc@users.noreply.github.com>
2020-09-03 15:08:40 -0400

    Update gtsam_core.cpp

--- src/gtsam_core.cpp
+++ src/gtsam_core.cpp
@@ -99,7 +99,8 @@ void GTSAM_CORE::initialize(Vector12& calibration, Vector3& position) {
     imu_params->gyroscopeCovariance = measured_omega_cov;       // gyro white noise in continuous
     imu_params->omegaCoriolis = w_coriolis;
 
-    current_summarized_measurement = nullptr;
+    current_summarized_measurement = boost::shared_ptr<PreintegratedImuMeasurements>(
+                                        new PreintegratedImuMeasurements(imu_params, current_bias));
   
     //ISAM2Params isam_params;
     ISAM2Params isam_params;
